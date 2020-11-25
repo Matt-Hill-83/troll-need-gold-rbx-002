0fd5b35ad5b0228276fc83482de4a79a2b2a5c4a
Matt Hill <matt.hill.email@gmail.com>
2020-11-25 07:45:38 -0800

    asdf

--- src/ServerScriptService/AddDialog/Dialog.lua
+++ src/ServerScriptService/AddDialog/Dialog.lua
@@ -4,22 +4,17 @@ local Utils = require(Sss.Source.Utils.Utils)
 
 local module = {}
 
-renderDialog = function(props)
+function module.renderDialog(props)
     local dialogConfigs = props.dialogConfigs
-    local clonedScene = props.clonedScene
-
     local dialogContainer = Utils.getFromTemplates("DialogTemplate")
-    -- local dialogContainer = Utils.getDescendantByName(clonedScene,
-    --                                                   "DialogTemplate")
 
     local renderTextsProps = {
         parent = dialogContainer,
         dialogConfigs = dialogConfigs
     }
-    Texts.renderTexts(renderTextsProps)
 
+    Texts.renderTexts(renderTextsProps)
     return dialogContainer
 end
 
-module.renderDialog = renderDialog
 return module
