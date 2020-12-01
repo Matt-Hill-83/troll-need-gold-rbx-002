dcd2fe363c1882fadbb31b701ea9abb2c6b31a52
Matt Hill <matt.hill.email@gmail.com>
2020-12-01 07:22:31 -0800

    update quests

--- src/ServerScriptService/QuestConfigs/QuestConfigs.lua
+++ src/ServerScriptService/QuestConfigs/QuestConfigs.lua
@@ -4,83 +4,6 @@ module.questConfigs = {
     {
         questTitle = "--- rbx-001",
         startSceneCoords = {row = 0, col = 0},
-        endSceneCoords = {col = 2, row = 0},
-        sceneConfigs = {
-            {
-                name = "home",
-                frames = {
-                    {
-                        dialogs = {
-                            {
-                                char = "dog01",
-                                text = "Did you see the space ship?"
-                            }, {char = "robby01", text = "I am new here..."}
-                        },
-                        characters01 = {{name = "dog01"}},
-                        characters02 = {{name = "robby01"}}
-                    }
-                },
-                coordinates = {row = 0, col = 0},
-                showBottomPath = false,
-                showRightPath = true,
-                showTopPath = false,
-                showLeftPath = false,
-                maxRow = 0,
-                maxCol = 0
-            }, {
-                name = "log",
-                frames = {
-                    {
-                        dialogs = {
-                            {
-                                char = "dog01",
-                                text = "Did you see the space ship?"
-                            }, {char = "robby01", text = "I am new here..."}
-                        },
-                        characters01 = {{name = "dog01"}},
-                        characters02 = {{name = "robby01"}}
-                    }
-                },
-                coordinates = {col = 1, row = 0},
-                showBottomPath = false,
-                showRightPath = true,
-                showTopPath = false,
-                showLeftPath = true,
-                maxRow = 0,
-                maxCol = 1
-            }, {
-                name = "cave",
-                frames = {
-                    {
-                        dialogs = {
-                            {
-                                char = "dog01",
-                                text = "Did you see the space ship?"
-                            }, {char = "robby01", text = "I am new here..."},
-                            {
-                                char = "trollKing01",
-                                text = "Mwa...  ha-ha ha-ha ha!!!"
-                            }
-                        },
-                        characters01 = {{name = "dog01"}},
-                        characters02 = {
-                            {name = "robby01"}, {name = "trollKing01"}
-                        }
-                    }
-                },
-                coordinates = {col = 2, row = 0},
-                showBottomPath = false,
-                showRightPath = false,
-                showTopPath = false,
-                showLeftPath = true,
-                maxRow = 0,
-                maxCol = 2
-            }
-        },
-        gridSize = {rows = 1, cols = 3}
-    }, {
-        questTitle = "--- rbx-002",
-        startSceneCoords = {row = 0, col = 0},
         endSceneCoords = {row = 0, col = 2},
         sceneConfigs = {
             {
@@ -88,13 +11,20 @@ module.questConfigs = {
                 frames = {
                     {
                         dialogs = {
-                            {
-                                char = "dog01",
-                                text = "Did you see the space ship?"
-                            }, {char = "robby01", text = "I am new here..."}
+                            {char = "kat", text = "Did you see the space ship?"},
+                            {char = "liz2", text = "I am new here..."},
+                            {char = "dog01", text = "I like bees"}
                         },
-                        characters01 = {{name = "dog01"}},
-                        characters02 = {{name = "robby01"}}
+                        characters01 = {{name = "dog01"}, {name = "kat"}},
+                        characters02 = {{name = "liz2"}}
+                    }, {
+                        dialogs = {
+                            {char = "kat", text = "yay bees"},
+                            {char = "dog01", text = "Bees are fun"},
+                            {char = "liz2", text = "We can play with bees!"}
+                        },
+                        characters01 = {{name = "dog01"}, {name = "kat"}},
+                        characters02 = {{name = "liz2"}}
                     }
                 },
                 coordinates = {row = 0, col = 0},
@@ -156,7 +86,7 @@ module.questConfigs = {
         },
         gridSize = {rows = 1, cols = 3}
     }, {
-        questTitle = "--- rbx-003",
+        questTitle = "--- rbx-002",
         startSceneCoords = {col = 0, row = 0},
         endSceneCoords = {col = 2, row = 0},
         sceneConfigs = {
@@ -196,6 +126,83 @@ module.questConfigs = {
                     }
                 },
                 coordinates = {row = 0, col = 1},
+                showBottomPath = false,
+                showRightPath = true,
+                showTopPath = false,
+                showLeftPath = true,
+                maxRow = 0,
+                maxCol = 1
+            }, {
+                name = "cave",
+                frames = {
+                    {
+                        dialogs = {
+                            {
+                                char = "dog01",
+                                text = "Did you see the space ship?"
+                            }, {char = "robby01", text = "I am new here..."},
+                            {
+                                char = "trollKing01",
+                                text = "Mwa...  ha-ha ha-ha ha!!!"
+                            }
+                        },
+                        characters01 = {{name = "dog01"}},
+                        characters02 = {
+                            {name = "robby01"}, {name = "trollKing01"}
+                        }
+                    }
+                },
+                coordinates = {col = 2, row = 0},
+                showBottomPath = false,
+                showRightPath = false,
+                showTopPath = false,
+                showLeftPath = true,
+                maxRow = 0,
+                maxCol = 2
+            }
+        },
+        gridSize = {rows = 1, cols = 3}
+    }, {
+        questTitle = "--- rbx-003",
+        startSceneCoords = {row = 0, col = 0},
+        endSceneCoords = {col = 2, row = 0},
+        sceneConfigs = {
+            {
+                name = "home",
+                frames = {
+                    {
+                        dialogs = {
+                            {
+                                char = "dog01",
+                                text = "Did you see the space ship?"
+                            }, {char = "robby01", text = "I am new here..."}
+                        },
+                        characters01 = {{name = "dog01"}},
+                        characters02 = {{name = "robby01"}}
+                    }
+                },
+                coordinates = {row = 0, col = 0},
+                showBottomPath = false,
+                showRightPath = true,
+                showTopPath = false,
+                showLeftPath = false,
+                maxRow = 0,
+                maxCol = 0
+            }, {
+                name = "log",
+                frames = {
+                    {
+                        dialogs = {
+                            {
+                                char = "dog01",
+                                text = "Did you see the space ship?"
+                            }, {char = "robby01", text = "I am new here..."}
+                        },
+                        characters01 = {{name = "dog01"}},
+                        characters02 = {{name = "robby01"}}
+                    }
+                },
+                coordinates = {col = 1, row = 0},
                 showBottomPath = false,
                 showRightPath = true,
                 showTopPath = false,
