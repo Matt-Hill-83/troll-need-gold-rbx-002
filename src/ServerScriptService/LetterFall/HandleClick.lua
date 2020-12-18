950b8642177117068afd1a8651780fb4719027a3
Matt Hill <matt.hill.email@gmail.com>
2020-12-18 06:27:49 -0800

    asdf

---
+++ src/ServerScriptService/LetterFall/HandleClick.lua
@@ -0,0 +1,101 @@
+local CS = game:GetService("CollectionService")
+local Sss = game:GetService("ServerScriptService")
+local Utils = require(Sss.Source.Utils.U001GeneralUtils)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local remoteEvent = ReplicatedStorage:WaitForChild("ClickBlockRE")
+local module = {}
+
+function colorLetterText(props)
+    local color = props.color
+    local letterBlock = props.letterBlock
+    local textLabels = Utils.getDescendantsByName(letterBlock, "BlockChar")
+    for i, label in ipairs(textLabels) do
+        label.TextColor3 = color or Color3.new(255, 0, 191)
+    end
+end
+function applyLetterText(props)
+    local char = props.char
+    local letterBlock = props.letterBlock
+    local textLabels = Utils.getDescendantsByName(letterBlock, "BlockChar")
+    for i, label in ipairs(textLabels) do label.Text = char end
+end
+function createBalls(miniGameState)
+    local letterFallFolder = miniGameState.letterFallFolder
+    local ball = Utils.getFirstDescendantByName(letterFallFolder, "GemTemplate")
+    for count = 1, 10 do
+        local newBall = ball:Clone()
+        newBall.Handle.CFrame = newBall.Handle.CFrame + Vector3.new(-0.1, 0, 0)
+        newBall.Parent = ball.Parent
+    end
+end
+function getRunTimeLetterFolder(miniGameState)
+    local letterFallFolder = miniGameState.letterFallFolder
+    local runtimeFolder = Utils.getOrCreateFolder(
+                              {
+            name = "RunTimeFolder",
+            parent = letterFallFolder
+        })
+    return Utils.getOrCreateFolder({
+        name = "RunTimeLetterRackFolder",
+        parent = runtimeFolder
+    })
+end
+function getWordFolder(miniGameState)
+    local letterFallFolder = miniGameState.letterFallFolder
+    local runtimeFolder = Utils.getOrCreateFolder(
+                              {
+            name = "RunTimeFolder",
+            parent = letterFallFolder
+        })
+    return (Utils.getOrCreateFolder({
+        name = "RunTimeWordBoxFolder",
+        parent = runtimeFolder
+    }))
+end
+function isDesiredLetter(letter, clickedLetter)
+    local textLabel = Utils.getFirstDescendantByName(clickedLetter, "BlockChar")
+                          .Text
+    return letter.found ~= true and letter.char == textLabel
+end
+function isWordComplete(wordLetters)
+    for i, word in ipairs(wordLetters) do
+        if not word.found then return false end
+    end
+    return true
+end
+function initClickHandler(miniGameState)
+    -- Gets arguments from EventHandler in StarterPack
+    function brickClickHandler(player, clickedLetter)
+        handleBrick(player, clickedLetter, miniGameState)
+    end
+    remoteEvent.OnServerEvent:Connect(brickClickHandler)
+end
+function handleBrick(player, clickedLetter, miniGameState)
+    local letterFallFolder = miniGameState.letterFallFolder
+    local wordLetters = miniGameState.wordLetters
+    local ballPitBottom = Utils.getFirstDescendantByName(letterFallFolder,
+                                                         "BallPitBottom")
+    if ballPitBottom then ballPitBottom:Destroy() end
+    local isChild = clickedLetter:IsDescendantOf(letterFallFolder)
+    if not isChild then return {} end
+    for i, letter in ipairs(wordLetters) do
+        if isDesiredLetter(letter, clickedLetter) then
+            letter.found = true
+            module.colorLetterText({
+                letterBlock = letter.instance,
+                color = Color3.fromRGB(113, 17, 161)
+            })
+            clickedLetter:Destroy()
+            local wordComplete = isWordComplete(wordLetters)
+            if wordComplete then
+                miniGameState.lastWordIndex = miniGameState.lastWordIndex + 1
+                module.initWord(miniGameState)
+            end
+            break
+        end
+    end
+end
+module.colorLetterText = colorLetterText
+module.createBalls = createBalls
+module.applyLetterText = applyLetterText
+return module
--- src/ServerScriptService/LetterFall/LetterFall.lua
+++ src/ServerScriptService/LetterFall/LetterFall.lua
@@ -3,6 +3,7 @@ local Sss = game:GetService("ServerScriptService")
 local Utils = require(Sss.Source.Utils.U001GeneralUtils)
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local remoteEvent = ReplicatedStorage:WaitForChild("ClickBlockRE")
+local LetterFall = require(Sss.Source.LetterFall.HandleClick)
 
 local module = {
     tagNames = {WordLetter = "WordLetter", LetterBlock = "LetterBlock"}
