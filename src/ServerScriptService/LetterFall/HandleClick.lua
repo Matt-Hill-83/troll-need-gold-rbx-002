60997eab2a93ae06078dad051b8e0ce4fc6c87aa
Matt Hill <matt.hill.email@gmail.com>
2020-12-18 06:59:37 -0800

    asdf

--- src/ServerScriptService/LetterFall/HandleClick.lua
+++ src/ServerScriptService/LetterFall/HandleClick.lua
@@ -3,6 +3,7 @@ local Sss = game:GetService("ServerScriptService")
 local Utils = require(Sss.Source.Utils.U001GeneralUtils)
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local remoteEvent = ReplicatedStorage:WaitForChild("ClickBlockRE")
+local InitWord = require(Sss.Source.LetterFall.InitWord)
 local module = {}
 
 function colorLetterText(props)
@@ -89,7 +90,7 @@ function handleBrick(player, clickedLetter, miniGameState)
             local wordComplete = isWordComplete(wordLetters)
             if wordComplete then
                 miniGameState.lastWordIndex = miniGameState.lastWordIndex + 1
-                module.initWord(miniGameState)
+                InitWord.initWord(miniGameState)
             end
             break
         end
