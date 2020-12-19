58d108d42200a3171ee7df385231a8f1e9ad1ea1
Matt Hill <matt.hill.email@gmail.com>
2020-12-19 10:30:57 -0800

    multiple words now working

--- src/ServerScriptService/LetterFall/InitWord.lua
+++ src/ServerScriptService/LetterFall/InitWord.lua
@@ -8,44 +8,35 @@ local module = {}
 function initWord(miniGameState)
     local letterFallFolder = miniGameState.letterFallFolder
     local wordFolder = getWordFolder(miniGameState)
-    -- local word = miniGameState.words[miniGameState.lastWordIndex]
 
     local words2 = {miniGameState.words[1], miniGameState.words[2]}
     local wordBoxFolder = Utils.getFirstDescendantByName(letterFallFolder,
                                                          "WordBoxFolder")
 
-    for wordIndex, word in ipairs(words2) do
-        -- for i, word in ipairs(miniGameState.words) do
-        --    
-        --    
+    for i, letter in ipairs(miniGameState.wordLetters) do
+        if letter.instance then letter.instance:Destroy() end
+        miniGameState.wordLetters[i] = nil
+    end
+    Utils.clearTable(miniGameState.wordLetters)
+
+    -- for wordIndex, word in ipairs(words2) do
+    for wordIndex, word in ipairs(miniGameState.words) do
         local newWordBoxFolder = wordBoxFolder:Clone()
         newWordBoxFolder.Parent = wordBoxFolder.Parent
         local letterBlockTemplate = Utils.getFirstDescendantByName(
                                         newWordBoxFolder, "LetterBlockTemplate")
+        local wordSpacingY = letterBlockTemplate.Size.Y * 1.1
 
         local baseWeld = Utils.getFirstDescendantByName(newWordBoxFolder,
                                                         "BaseWeld")
 
-        print('baseWeld' .. ' - start');
-        print(baseWeld);
         baseWeld.Enabled = false
         local newWordBox = Utils.getFirstDescendantByName(newWordBoxFolder,
                                                           "WordBox")
 
-        -- local welds = Utils.getDescendantsByType(newWordBox, "Weld")
-        -- for i, weld in ipairs(welds) do
-        --     weld.Enabled = false
-        --     -- 
-        -- end
-
-        for i, letter in ipairs(miniGameState.wordLetters) do
-            if letter.instance then letter.instance:Destroy() end
-            miniGameState.wordLetters[i] = nil
-        end
-        Utils.clearTable(miniGameState.wordLetters)
-
         newWordBox.PrimaryPart.CFrame = newWordBox.PrimaryPart.CFrame +
-                                            Vector3.new(0, 20 * wordIndex, 0)
+                                            Vector3.new(0, wordSpacingY *
+                                                            wordIndex, 0)
 
         newWordBox.Name = newWordBox.Name .. "zzz" .. wordIndex
         local weldPlate = Utils.getFirstDescendantByName(newWordBoxFolder,
@@ -56,39 +47,37 @@ function initWord(miniGameState)
                                      "WordLetterBlockPositioner")
 
         letterPositioner.Name = letterPositioner.Name .. "-" .. wordIndex
-        print('letterPositioner.Name' .. ' - start');
-        print(letterPositioner.Name);
 
         letterBlockTemplate.Transparency = 1
         local spacingFactor = 1.05
 
         Utils.enableChildWelds({part = letterBlockTemplate, enabled = false})
 
-        -- for letterIndex = 1, #word do
-        --     local letter = string.sub(word, letterIndex, letterIndex + 0)
-        --     local newLetter = letterBlockTemplate:Clone()
-        --     newLetter.Name = "wordLetter-" .. letterIndex
-        --     newLetter.Transparency = 0
-        --     Utils.enableChildWelds({part = newLetter, enable = true})
+        for letterIndex = 1, #word do
+            local letter = string.sub(word, letterIndex, letterIndex + 0)
+            local newLetter = letterBlockTemplate:Clone()
+            newLetter.Name = "wordLetter-" .. letterIndex
+            newLetter.Transparency = 0
+            Utils.enableChildWelds({part = newLetter, enable = true})
 
-        --     local z = newLetter.Size.Z * (letterIndex - 1) * spacingFactor
-        --     letterPositioner.Transparency = 1
+            local z = newLetter.Size.Z * (letterIndex - 1) * spacingFactor
+            letterPositioner.Transparency = 1
 
-        --     CS:AddTag(newLetter, LetterFallUtils.tagNames.WordLetter)
-        --     LetterFallUtils.applyLetterText(
-        --         {letterBlock = newLetter, char = letter})
-        --     LetterFallUtils.colorLetterText(
-        --         {letterBlock = newLetter, color = Color3.new(255, 0, 191)})
+            CS:AddTag(newLetter, LetterFallUtils.tagNames.WordLetter)
+            LetterFallUtils.applyLetterText(
+                {letterBlock = newLetter, char = letter})
+            LetterFallUtils.colorLetterText(
+                {letterBlock = newLetter, color = Color3.new(255, 0, 191)})
 
-        --     newLetter.CFrame = letterPositioner.CFrame *
-        --                            CFrame.new(Vector3.new(0, 0, z))
-        --     -- Do this last to avoid tweening
-        --     newLetter.Parent = wordFolder
-        --     table.insert(miniGameState.wordLetters,
-        --                  {char = letter, found = false, instance = newLetter})
+            newLetter.CFrame = letterPositioner.CFrame *
+                                   CFrame.new(Vector3.new(0, 0, z))
+            -- Do this last to avoid tweening
+            newLetter.Parent = wordFolder
+            table.insert(miniGameState.wordLetters,
+                         {char = letter, found = false, instance = newLetter})
 
-        -- end
-        baseWeld.Enabled = true
+        end
+        -- baseWeld.Enabled = true
         newWordBox.PrimaryPart.Anchored = true
     end
     wordBoxFolder:Destroy()
