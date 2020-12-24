e92f5b352ce2544a066e7264604455586e8d0b95
Matt Hill <matt.hill.email@gmail.com>
2020-12-23 18:28:56 -0800

    asdf

--- src/ServerScriptService/AddDialog/Dialog.lua
+++ src/ServerScriptService/AddDialog/Dialog.lua
@@ -1,16 +1,10 @@
-local Sss = game:GetService("ServerScriptService")
-local Texts = require(Sss.Source.AddDialog.Texts)
-
-local module = {}
-
-function module.renderDialog(props)
-    local dialogConfigs = props.dialogConfigs
-    local sgui = props.sgui
-
-    local renderTextsProps = {dialogConfigs = dialogConfigs, sgui = sgui}
-
-    Texts.renderTexts(renderTextsProps)
-
-end
-
-return module
+-- local Sss = game:GetService("ServerScriptService")
+-- local Texts = require(Sss.Source.AddDialog.Texts)
+-- local module = {}
+-- function module.renderDialog(props)
+--     local dialogConfigs = props.dialogConfigs
+--     local sgui = props.sgui
+--     local renderTextsProps = {dialogConfigs = dialogConfigs, sgui = sgui}
+--     Texts.renderTexts(renderTextsProps)
+-- end
+-- return module
