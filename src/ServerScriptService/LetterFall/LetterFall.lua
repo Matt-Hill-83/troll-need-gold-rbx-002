393b9307225d45020cfb3d6a8cf54b46b8220c0b
Matt Hill <matt.hill.email@gmail.com>
2020-12-26 16:58:22 -0800

    works

--- src/ServerScriptService/LetterFall/LetterFall.lua
+++ src/ServerScriptService/LetterFall/LetterFall.lua
@@ -11,6 +11,39 @@ local LetterFallUtils = require(Sss.Source.LetterFall.LetterFallUtils)
 local remoteEvent = ReplicatedStorage:WaitForChild("ClickBlockRE")
 local module = {}
 
+function configCouchTrigger(miniGameState)
+    local letterFallFolder = miniGameState.letterFallFolder
+    local seat = Utils.getFirstDescendantByName(letterFallFolder, "LFCouchSeat")
+
+    seat:GetPropertyChangedSignal("Occupant"):Connect(
+        function(miniGameState)
+            local cameraPath1 = Utils.getFirstDescendantByName(letterFallFolder,
+                                                               "ScreenCameraPath1")
+            local cameraPath2 = Utils.getFirstDescendantByName(letterFallFolder,
+                                                               "ScreenCameraPath2")
+
+            local humanoid = seat.Occupant
+            if humanoid then
+                local player = Utils.getPlayerFromHumanoid(humanoid)
+
+                if player then
+                    currentPlayer = player
+                    letterFallFreezeCameraRE:FireClient(player, cameraPath1,
+                                                        cameraPath2, true)
+                    return
+                end
+            end
+
+            -- player leaves seat
+            if currentPlayer then
+                letterFallFreezeCameraRE:FireClient(currentPlayer, cameraPath1,
+                                                    cameraPath2, false)
+                currentPlayer = nil
+            end
+        end)
+
+end
+
 function initGameToggle(miniGameState)
     local letterFallFolder = miniGameState.letterFallFolder
     local startGameTrigger = Utils.getFirstDescendantByName(letterFallFolder,
@@ -24,12 +57,16 @@ function initGameToggle(miniGameState)
     function onPartTouched(otherPart)
         if not miniGameState.initCompleted then
             miniGameState.initCompleted = true
-            -- HandleClick.initClickHandler(miniGameState)
 
+            HandleClick.initClickHandler(miniGameState)
+            LetterFallUtils.createBalls(miniGameState)
+            configCouchTrigger(miniGameState)
         end
     end
+
+    -- Do this so that all letterFall in all scenes are not jiggling and looking
+    -- for clicks until they are needed
     startGameTrigger.Touched:Connect(onPartTouched)
-    -- LetterFallUtils.createBalls(miniGameState)
 end
 
 module.initGameToggle = initGameToggle
