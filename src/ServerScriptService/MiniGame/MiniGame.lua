902ca8f9be312abc816cb4fa4dad1f29fa4464c5
Matt Hill <matt.hill.email@gmail.com>
2020-12-30 03:54:21 -0800

    asdf

--- src/ServerScriptService/MiniGame/MiniGame.lua
+++ src/ServerScriptService/MiniGame/MiniGame.lua
@@ -53,15 +53,15 @@ function module.addMiniGame(props)
 
         local letterFallPart = clonedLetterFallModel.PrimaryPart
 
-        local offsetZ = -letterFallPart.Size.Z / 2
-        local offsetY = -letterFallPart.Size.Y / 2
+        -- local offsetX = -(letterFallPart.Size.X - parent.Size.X) / 2
+        local offsetX = 0
+        local offsetY = -(letterFallPart.Size.Y + parent.Size.Y) / 2
+        local offsetZ = -(letterFallPart.Size.Z + parent.Size.Z) / 2
 
-        local offsetCFrame = CFrame.new(0, offsetY, offsetZ)
+        local offsetCFrame = CFrame.new(offsetX, offsetY, offsetZ)
+        letterFallPart.CFrame = parent.CFrame:ToWorldSpace(offsetCFrame)
 
-        clonedLetterFallModel.PrimaryPart.CFrame =
-            parent.CFrame:ToWorldSpace(offsetCFrame)
-
-        clonedLetterFallModel.PrimaryPart.Anchored = true
+        letterFallPart.Anchored = true
 
         miniGameState.letterFallFolder = letterFallFolder
         InitLetterFall.initLetterFall(miniGameState)
