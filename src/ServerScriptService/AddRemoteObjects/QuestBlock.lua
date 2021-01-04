33d488c6d61d394b58c69dc2cc21d425a0545e2d
Matt Hill <matt.hill.email@gmail.com>
2021-01-04 02:19:34 -0800

    asdf

--- src/ServerScriptService/AddRemoteObjects/QuestBlock.lua
+++ src/ServerScriptService/AddRemoteObjects/QuestBlock.lua
@@ -30,8 +30,8 @@ renderQuestBlock = function(props)
         child = dockPositioner,
         offsetConfig = {
             useParentNearEdge = Vector3.new(1, -1, -1),
-            -- useParentNearEdge = Vector3.new(1, -1, 0),
-            useChildNearEdge = Vector3.new(-1, -1, -1)
+            useChildNearEdge = Vector3.new(-1, -1, -1),
+            offsetAdder = Vector3.new(0, 0, -5)
         }
     }
 
